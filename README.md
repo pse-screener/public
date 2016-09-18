7b22b67e93f35db0102a96e717b7d705906d0496
jhunexjun <jdream_catcher@yahoo.com>
2016-09-18 18:55:00 +0800

    I can now login.

--- README.md
+++ README.md
@@ -36,3 +36,10 @@ Apache2 config
         CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 ```
+
+# HTTP Requests
+**Requesting token**
+curl http://www.pse-screener.com/oauth/token -d 'grant_type=password&username=test@gmail.com.com&password=123456&client_id=1&client_secret=mjy4eilKhSJPd8y4IkHUPxiYvzB3UMShxNyJGZVz'
+
+**New registration**
+$ curl -H "Accept: application/json " http://www.pse-screener.com/register -d "fName=Jhunex&lName=Jun&gender=M&email=test1@gmail.com&password=123456&mobileNo=09206939093&password_confirmation=123456" > /d/tmp/error.html
--- src/services/LoginDao.js
+++ src/services/LoginDao.js
@@ -7,7 +7,6 @@ export default {
 		data.client_id = publicVar.client_id;
 		data.client_secret = publicVar.client_secret;
 
-		// curl http://yourebusinessrest.com/mytoken.php -d 'grant_type=password&username=simply.amazing.wizard@gmail.com&password=1234'
 		let promise = new Promise(function(resolve, reject) {
 			$.ajax({
 	            type: "POST",
@@ -28,27 +27,10 @@ export default {
 			LoginActionCreator.onLoginSubmitDone(data);
 		}, function(reason) {
 			console.log('Error logging-in: ', reason);
+			window.location = publicVar.gotoUnsecuredLogin();
 		});
 	},
 	loginToAdmin: function(accessToken) {
-		//window.location = "https://www.yahoo.com";
 		window.location = publicVar.getUnsecuredEndpointWithoutIndex().concat('/admin/#/app/', accessToken);
-		/*let token = {access_token: accessToken};
-		$.ajax({
-            type: "POST",
-            //url: publicVar.getUnsecuredEndpointWithoutIndex().concat('/admin'),
-            url: "https://www.yahoo.com/",
-            data: token,
-            success: function(data) {
-            	if (data.success)
-            		window.location = "https://www.yahoo.com/";
-            		//window.location = publicVar.getUnsecuredAdminDomainAndPath().concat('/',accessToken);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-            	let error = {};
-            	error.jqXHR = jqXHR; error.textStatus = textStatus; error.errorThrown = errorThrown;
-                reject(error);
-            }
-        });*/
 	}
 };
