593aee22ef45cbbf1af63ed66693db333f196a1f
jhunexjun <jdream_catcher@yahoo.com>
2017-04-25 23:49:30 +0800

    Started to write real text for the home page.

--- src/components/HomeContents.js
+++ src/components/HomeContents.js
@@ -24,7 +24,7 @@ var Home = {
                         <div className="page-header">
                             <h4>PSE Monitoring Alert System</h4>
                         </div>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        <p>A solution to a problem in monitoring PSE.</p>
                     </div>
                 </div>);
     }
